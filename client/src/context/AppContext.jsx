6869489dd7212517643ce1555825e81dff88c397
Al Mun Tasir Mamun <almuntasirmamun2000@gmail.com>
2026-03-06 05:42:54 +0600

    fix: add fallback for axios baseURL if VITE_API_URL is missing

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -4,7 +4,7 @@ import { dummyProducts } from "../assets/assets";
 import toast from "react-hot-toast";
 import axios from "axios";
 axios.defaults.withCredentials = true;
-axios.defaults.baseURL = import.meta.env.VITE_API_URL;
+axios.defaults.baseURL = import.meta.env.VITE_API_URL || "https://e-commerce-production-0858.up.railway.app";
 
 export const AppContext = createContext(null);
 
